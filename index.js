8846a1e102fae0d79dd303c9e1c959c7f0602fb4
joshua-bell-sonrai <joshua.bell@sonraisecurity.com>
2023-01-21 12:17:54 -0400

    update

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const app = express();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(express.static('build'));
-app.use('/api', createProxyMiddleware({ target: process.env.API_ENDPOINT || 'http://localhost:5000'}));
+app.use('/api', createProxyMiddleware({ target: process.env.API_ENDPOINT || 'http://localhost:5000', changeOrigin: process.env.CHANGE_ORIGIN || false}));
 
 app.listen(port, () => console.log(`Frontend listening on port ${port}!`));
